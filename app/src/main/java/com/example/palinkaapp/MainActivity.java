293a69d0d6c3d6855b70dc10d0cfd297839588e0
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2021-11-24 17:55:36 +0100

    Init metódus

--- app/src/main/java/com/example/palinkaapp/MainActivity.java
+++ app/src/main/java/com/example/palinkaapp/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.palinkaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnListaz;
+    private Button btnFelvesz;
+    private Button btnKeres;
+    private SQLiteDBHelper adatbazis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        btnListaz = findViewById(R.id.btn_listazasa);
+        btnFelvesz = findViewById(R.id.btn_felvetel);
+        btnKeres = findViewById(R.id.btn_keresese);
+        adatbazis = new SQLiteDBHelper(this);
     }
 }
