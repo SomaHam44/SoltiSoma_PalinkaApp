46064c49c77b134023a30ecb5f4d54c6ffeaaa99
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2021-11-24 18:14:30 +0100

    MainActivity kialakítása

--- app/src/main/java/com/example/palinkaapp/MainActivity.java
+++ app/src/main/java/com/example/palinkaapp/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.palinkaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,17 +16,49 @@ public class MainActivity extends AppCompatActivity {
     private Button btnFelvesz;
     private Button btnKeres;
     private SQLiteDBHelper adatbazis;
+    private TextView textListank;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        textListank.setMovementMethod(new ScrollingMovementMethod());
 
         btnFelvesz.setOnClickListener(v -> {
             Intent felvetelre = new Intent(MainActivity.this, AdatFelvetelActivity.class);
             startActivity(felvetelre);
             finish();
+        });
+
+        btnKeres.setOnClickListener(v -> {
+            Intent keresesre = new Intent(MainActivity.this, AdatKeresesActivity.class);
+            startActivity(keresesre);
+            finish();
+        });
+
+        btnListaz.setOnClickListener(v -> {
+            Cursor adataink = adatbazis.listaz();
+            if (adataink.getCount() == 0) {
+                Toast.makeText(this, "Nincs felvéve adat az adatbázisban", Toast.LENGTH_SHORT).show();
+
+            }
+            else {
+                StringBuilder builder = new StringBuilder();
+                while (adataink.moveToNext()) {
+                    builder.append("ID: ").append(adataink.getInt(0));
+                    builder.append(System.lineSeparator());
+                    builder.append("Főző: ").append(adataink.getString(1));
+                    builder.append(System.lineSeparator());
+                    builder.append("Gyümölcs: ").append(adataink.getString(2));
+                    builder.append(System.lineSeparator());
+                    builder.append("Alkohol: ").append(adataink.getInt(3));
+                    builder.append(System.lineSeparator());
+                    builder.append(System.lineSeparator());
+
+                }
+                textListank.setText(builder.toString());
+            }
         });
     }
 
@@ -31,5 +67,6 @@ public class MainActivity extends AppCompatActivity {
         btnFelvesz = findViewById(R.id.btn_felvetel);
         btnKeres = findViewById(R.id.btn_keresese);
         adatbazis = new SQLiteDBHelper(this);
+        textListank = findViewById(R.id.text_lista);
     }
 }
